dd21a08f14a37d63787a9d51eb985187375f1249
anjoo-k <knjou92@naver.com>
2025-06-17 15:22:49 +0900

    feat, chore : ReceiveWorker의 sendCommand() 구현, receiveFileList() 구현, ClientService로 makeDir() 이동

--- nest_clinet_0616/src/org/kosa/nest/network/ReceiveWorker.java
+++ nest_clinet_0616/src/org/kosa/nest/network/ReceiveWorker.java
@@ -6,13 +6,15 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.ObjectInputStream;
 import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.kosa.nest.common.ClientConfig;
+import org.kosa.nest.model.FileVO;
 
 public class ReceiveWorker {
 
@@ -33,24 +35,16 @@ public class ReceiveWorker {
         socket = new Socket(ip, 1234);
     }
 
-    /**
-     * 클라이언트가 파일 다운로드를 시작할 때 <br>
-     * 파일을 다운로드받을 경로 있는지 체크 후 <br>
-     * 없으면 경로 생성 <br>
-     */
-    public void makeDir() {
-        File file = new File(ClientConfig.REPOPATH);
-        if (!file.isDirectory())
-            file.mkdirs();
-    }
 
-    // 이거 불린에 관련된 것도 이야기해 봐야 겠다. -> void로 향후 변경
     /**
      * 클라이언트가 입력한 명령어를 서버로 전달 <br>
      * @param command
      * @return
      * @throws IOException
      */
+    // 이거 불린에 관련된 것도 이야기해 봐야 겠다. -> void로 향후 변경
+    // 이 명령어를 보내서 명령어에 대한 값을 받을 때 download인지 receiveFileList인지
+    // 선택해야하는데 어디서?
     public boolean sendCommand(String command) throws IOException {
         
         this.command = command;  // 명령어 가져다 쓸 수 있게 세팅
@@ -63,13 +57,37 @@ public class ReceiveWorker {
             if (bw != null)
                 bw.close();
         }
-
         return true;
+    }
+    
+    /**
+     * 명령어 : search, info
+     * 클라이언트가 search, info 명령을 내리면 <br>
+     * 파일 list를 server로부터 받아 FileVO로 만들고 list에 넣어서 반환 <br>
+     * @return
+     * @throws IOException 
+     * @throws ClassNotFoundException 
+     */
+    // 메서드 이름 receiveResult 에서 receiveFileList로
+    public ArrayList<FileVO> receiveFileList() throws IOException, ClassNotFoundException{
+        List<FileVO> list = new ArrayList<>();
+        ObjectInputStream ois = null;
+        
+        try {
+            ois = new ObjectInputStream(socket.getInputStream());
+            list = (ArrayList)ois.readObject();
+        }finally {
+            if(ois != null)
+                ois.close();
+        }
+        
+        return (ArrayList)list;
     }
 
     /**
+     * 명령어 : download
      * 클라이언트가 download 명령어로 다운로드 받을 파일 제목을 명령하면 <br>
-     * 파일을 서버로부터 받아 클라이언트의 컴퓨터에 다운로드 하는 기능
+     * 파일을 서버로부터 받아 클라이언트의 컴퓨터에 다운로드 하는 기능 <br>
      * 
      * @return
      * @throws IOException
@@ -78,24 +96,23 @@ public class ReceiveWorker {
     public boolean downloadFile() throws IOException {
 
         /*
-         * 1. 파일들이 저장될 폴더가 디렉토리에 있는지 확인 후, 없으면 만들어야
+         * 1. 파일들이 저장될 폴더가 디렉토리에 있는지 확인 후, 없으면 만들어야 -> Service로 옮김
          * 2. 파일을 서버로부터 받아와서
          * 3. 내 컴퓨터 경로에 다운로드,
          * 이 때 byteStream 사용 8192
          */
 
-        makeDir();
-
-        OutputStream fos = null;
         BufferedOutputStream bos = null;
         BufferedInputStream bis = null;
 
         try {
-            bis = new BufferedInputStream(socket.getInputStream());
+            // 서버에서 다운로드 받을 파일을 받아옴
+            bis = new BufferedInputStream(socket.getInputStream(), 8192);
             // tmpFileName은 나중에 서버에서 받아서 처리
-            bos = new BufferedOutputStream(
-                    new FileOutputStream(ClientConfig.REPOPATH + File.separator + "tmpFileName"));
+            // 내 컴퓨터에 파일로 저장.....
+            bos = new BufferedOutputStream(new FileOutputStream(ClientConfig.REPOPATH + File.separator + "tmpFileName"), 8192); 
 
+            // 바이트코드인 파일을 한 줄 씩 읽어서 쓰기
             int data = bis.read();
             while (data != -1) {
                 bos.write(data);
@@ -104,8 +121,8 @@ public class ReceiveWorker {
         } finally {
             if (bos != null)
                 bos.close();
-            if (fos != null)
-                fos.close();
+            if (bis != null)
+                bis.close();
         }
         return true;
     }
--- nest_clinet_0616/src/org/kosa/nest/service/ClientService.java
+++ nest_clinet_0616/src/org/kosa/nest/service/ClientService.java
@@ -11,6 +11,20 @@ import org.kosa.nest.network.ReceiveWorker;
 public class ClientService {
     
     private ReceiveWorker receiveWorker;
+    // 임시 명령어
+    private String command = "download";
+    
+    /**
+     * 클라이언트가 파일 다운로드를 시작할 때 <br>
+     * 파일을 다운로드받을 경로 있는지 체크 후 <br>
+     * 없으면 경로 생성 <br>
+     */
+    public void makeDir() {
+        File file = new File(ClientConfig.REPOPATH);
+        if (!file.isDirectory())
+            file.mkdirs();
+    }
+
  
     /**
      *  파일 다운로드가 성공하면 boolean값 받아 리턴
@@ -18,8 +32,10 @@ public class ClientService {
      * @return
      * @throws IOException
      */
-    public boolean download(String command) throws IOException {
-        return receiveWorker.downloadFile(command);
+    public boolean download() throws IOException {
+        makeDir();
+        receiveWorker.sendCommand(command);
+        return receiveWorker.downloadFile();
     }
 
 }
