604811ea8fba47aa3a39d676ae748601f4a226bc
anjoo-k <knjou92@naver.com>
2025-06-17 14:06:37 +0900

    feat : ReceiveWorker에 connection 위한 소켓 세팅, sendCommand() 메서드 구현, command 인스턴스 변수로 설정

--- nest_clinet_0616/src/org/kosa/nest/network/ReceiveWorker.java
+++ nest_clinet_0616/src/org/kosa/nest/network/ReceiveWorker.java
@@ -2,19 +2,37 @@ package org.kosa.nest.network;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import org.kosa.nest.common.ClientConfig;
 
 public class ReceiveWorker {
-    
+
     private Socket socket;
-    
+
+    // 임시 IP
+    private String ip = "127.0.0.1";
+    private String command;
+
+    /**
+     * ReceiveWorker 생성자로 socket 생성해 서버와 연결 <br>
+     * 
+     * @throws UnknownHostException
+     * @throws IOException
+     */
+    // getNetwork() 필요없고 이렇게 하면 되지 않나?
+    public ReceiveWorker() throws UnknownHostException, IOException {
+        socket = new Socket(ip, 1234);
+    }
+
     /**
      * 클라이언트가 파일 다운로드를 시작할 때 <br>
      * 파일을 다운로드받을 경로 있는지 체크 후 <br>
@@ -22,48 +40,73 @@ public class ReceiveWorker {
      */
     public void makeDir() {
         File file = new File(ClientConfig.REPOPATH);
-        if(!file.isDirectory())
+        if (!file.isDirectory())
             file.mkdirs();
     }
-    
+
+    // 이거 불린에 관련된 것도 이야기해 봐야 겠다. -> void로 향후 변경
+    /**
+     * 클라이언트가 입력한 명령어를 서버로 전달 <br>
+     * @param command
+     * @return
+     * @throws IOException
+     */
+    public boolean sendCommand(String command) throws IOException {
+        
+        this.command = command;  // 명령어 가져다 쓸 수 있게 세팅
+        BufferedWriter bw = null;
+
+        try {
+            bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bw.write(command);
+        } finally {
+            if (bw != null)
+                bw.close();
+        }
+
+        return true;
+    }
+
     /**
      * 클라이언트가 download 명령어로 다운로드 받을 파일 제목을 명령하면 <br>
      * 파일을 서버로부터 받아 클라이언트의 컴퓨터에 다운로드 하는 기능
-     * @param command
+     * 
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
-    public boolean downloadFile(String command) throws IOException {
-        
+    // 이거 불린에 관련된 것도 이야기해 봐야 겠다. -> void로 향후 변경
+    public boolean downloadFile() throws IOException {
+
         /*
          * 1. 파일들이 저장될 폴더가 디렉토리에 있는지 확인 후, 없으면 만들어야
          * 2. 파일을 서버로부터 받아와서
-         * 3. 내 컴퓨터 경로에 다운로드, 이 때 byteStream 사용
-         * 8192
+         * 3. 내 컴퓨터 경로에 다운로드,
+         * 이 때 byteStream 사용 8192
          */
-        
-        makeDir();  // 1.
-        
+
+        makeDir();
+
         OutputStream fos = null;
         BufferedOutputStream bos = null;
         BufferedInputStream bis = null;
 
         try {
             bis = new BufferedInputStream(socket.getInputStream());
-            // tmpFileName은 
-            bos = new BufferedOutputStream(new FileOutputStream(ClientConfig.REPOPATH + File.separator + "tmpFileName"));
-            
+            // tmpFileName은 나중에 서버에서 받아서 처리
+            bos = new BufferedOutputStream(
+                    new FileOutputStream(ClientConfig.REPOPATH + File.separator + "tmpFileName"));
+
             int data = bis.read();
-            while(data != -1) {
+            while (data != -1) {
                 bos.write(data);
                 data = bis.read();
             }
-        }finally {
-            if(bos != null)
+        } finally {
+            if (bos != null)
                 bos.close();
-            if(fos != null)
+            if (fos != null)
                 fos.close();
         }
-        return false;
+        return true;
     }
 }
