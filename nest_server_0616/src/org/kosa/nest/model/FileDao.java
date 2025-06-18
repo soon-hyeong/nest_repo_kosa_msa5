995667088780a4593d73ec5d6e7c2535950f845b
soon-hyeong <shjoo820@naver.com>
2025-06-18 11:51:54 +0900

    feat: Server 프로그램의 FileDao의 Create, Update, Delete 기능을 구현

--- nest_server_0616/src/org/kosa/nest/model/FileDao.java
+++ nest_server_0616/src/org/kosa/nest/model/FileDao.java
@@ -4,11 +4,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
+
 import org.kosa.nest.common.DatabaseUtil;
 
 public class FileDao {
+	
     public List<FileVO> getFileinfoList() throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -33,5 +38,63 @@ public class FileDao {
         }
 
         return list;
+    }
+    
+    public void createFileInfo(FileVO fileVO) throws SQLException {
+    	Connection con = null;
+    	PreparedStatement pstmt = null;
+    	
+    	try {
+    		con = DatabaseUtil.getConnection();
+    		String sql = "INSERT INTO file (uploaded_time, title, tag, description, admin_id, created_at, file_location) "
+    				+ " VALUES(?, ?, ?, ?, ?, ?, ?)";
+    		pstmt = con.prepareStatement(sql);
+    		pstmt.setTimestamp(1, Timestamp.valueOf(fileVO.getUploadAt()));
+    		pstmt.setString(2, fileVO.getSubject());
+    		pstmt.setString(3, fileVO.getTag());
+    		pstmt.setString(4, fileVO.getDescription());
+    		pstmt.setInt(5, fileVO.getAdminId());
+    		pstmt.setTimestamp(6, Timestamp.valueOf(fileVO.getCreatedAt()));
+    		pstmt.setString(7, fileVO.getSubject());    		
+    		
+    	} finally {
+    		DatabaseUtil.closeAll(pstmt, con);
+    	}
+    }
+    
+    public void updateFileInfo(FileVO fileVO) throws SQLException {
+    	Connection con = null;
+    	PreparedStatement pstmt = null;
+    	
+    	try {
+    		con = DatabaseUtil.getConnection();
+    		// 파일 정보 업데이트시 제목, 이름, 태그, 설명, 관리자아이디, 파일위치만 변경 가능
+    		String sql = "UPDATE file SET title = ?, tag = ?, description = ?, admin_id = ?, file_location = ? WHERE title = ?";
+    		pstmt = con.prepareStatement(sql);
+    		pstmt.setString(1, fileVO.getSubject());
+    		pstmt.setString(2, fileVO.getTag());
+    		pstmt.setString(3, fileVO.getDescription());
+    		pstmt.setInt(4, fileVO.getAdminId());
+    		pstmt.setString(5, fileVO.getFileLocation());
+    		pstmt.setString(6, fileVO.getSubject());
+    		pstmt.executeUpdate();
+    	} finally {
+    		DatabaseUtil.closeAll(pstmt, con);
+    	}
+    }
+    
+    public void deleteFileInfo(FileVO fileVO) throws SQLException {
+    	Connection con = null;
+    	PreparedStatement pstmt = null;
+    	
+    	try {
+    		con = DatabaseUtil.getConnection();
+    		String sql = "DELETE FROM file WHERE subject = ?";
+    		pstmt = con.prepareStatement(sql);
+    		pstmt.setString(1, fileVO.getSubject());
+    		pstmt.executeQuery();
+    	} finally{
+    		DatabaseUtil.closeAll(pstmt, con);
+    	}
     }
 }
